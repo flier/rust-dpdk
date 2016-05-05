b5dc7a9b84b6f74d5d56e5006f1db68416347f17
Flier Lu <flier.lu@nexusguard.com>
2016-05-05 12:16:10 +0800

    parse macro defines with regex

--- rte-sys/build.rs
+++ rte-sys/build.rs
@@ -2,6 +2,7 @@
 extern crate log;
 extern crate env_logger;
 extern crate gcc;
+extern crate regex;
 
 use std::env;
 use std::env::consts::*;
@@ -30,14 +31,26 @@ fn build_dpdk(base_dir: &PathBuf) {
 }
 
 fn gen_rte_config(base_dir: &PathBuf) {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_dir = PathBuf::from(env::var("OUT_DIR")
+                                    .expect("environment variable `OUT_DIR` not defined"));
     let dest_path = out_dir.join("rte_config.rs");
 
     debug!("generating rte_config.rs @ {}", out_dir.to_str().unwrap());
 
-    let mut cmd = gcc::Config::new().flag("-dM").flag("-E").get_compiler().to_command();
+    let mut cmd = gcc::Config::new()
+                      .include(base_dir.join("include"))
+                      .flag("-dM")
+                      .flag("-E")
+                      .get_compiler()
+                      .to_command();
 
-    cmd.arg(base_dir.join("include").join("rte_config.h"));
+    cmd.arg("src/rte.h");
+
+    let re_num = regex::Regex::new(r"^\(?(?:\([:word:]+\))?\(?(?P<value>-?[:digit:]+)(?P<suffix>[Uu]?[Ll]*)\)?$")
+                     .unwrap();
+    let re_hex = regex::Regex::new(r"^\(?(?:\([:word:]+\))?\(?(?P<value>0x[:xdigit:]+)(?P<suffix>[Uu]?[Ll]*)\)?$")
+                     .unwrap();
+    let re_shift = regex::Regex::new(r"^\(?(?P<value>[:digit:]+\s*<<\s*[:digit:]+)\)?$").unwrap();
 
     debug!("executing: {:?}", cmd);
 
@@ -46,6 +59,15 @@ fn gen_rte_config(base_dir: &PathBuf) {
 
     let f = File::create(&dest_path).unwrap();
 
+    fn value_types(sign: bool, long: bool) -> &'static str {
+        match (sign, long) {
+            (true, true) => "i64",
+            (false, true) => "u64",
+            (true, false) => "i32",
+            (false, false) => "u32",
+        }
+    }
+
     for line in Cursor::new(output.stdout)
                     .lines()
                     .map(|r| r.unwrap())
@@ -53,20 +75,39 @@ fn gen_rte_config(base_dir: &PathBuf) {
         let vars: Vec<&str> = line.splitn(3, " ").collect();
 
         let name = vars[1];
-        let value = vars[2];
+        let mut value = vars[2];
 
-        write!(&f,
-               "pub const {} : {} = {};\n",
-               name,
-               if value.starts_with("\"") && value.ends_with("\"") {
-                   "&'static str"
-               } else if value.starts_with("-") {
-                   "i32"
-               } else {
-                   "u32"
-               },
-               value)
-            .unwrap();
+        if let Some(value_type) = if value.starts_with("\"") && value.ends_with("\"") {
+            Some("&'static str")
+        } else if let Some(caps) = re_num.captures(value) {
+            value = caps.name("value").expect("`value` not found");
+
+            Some(value_types(value.starts_with("-"),
+                             caps.name("suffix")
+                                 .map_or_else(|| false, |suffix| suffix.to_uppercase() == "ULL")))
+        } else if let Some(caps) = re_hex.captures(value) {
+            value = caps.name("value").expect("`value` not found");
+
+            Some(value_types(value.starts_with("-"),
+                             caps.name("suffix")
+                                 .map_or_else(|| false, |suffix| suffix.to_uppercase() == "ULL")))
+        } else if let Some(caps) = re_shift.captures(value) {
+            value = caps.name("value").expect("`value` not found");
+
+            Some("u32")
+        } else {
+            None
+        } {
+            write!(&f,
+                   "pub const {}: {} = {}; // {}\n",
+                   name,
+                   value_type,
+                   value,
+                   vars[2])
+        } else {
+            write!(&f, "// #define {} {}\n", name, value)
+        }
+        .unwrap();
     }
 }
 
